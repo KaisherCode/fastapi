9fb28b5313881f365c598494a9d2f9ea3524df8b
Wilson Dev <wilson.hch@hynkor.com>
2024-03-30 11:01:49 -0300

    Método GET en FastAPI

--- my_movie_api/main.py
+++ my_movie_api/main.py
@@ -1,4 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+movies=[
+    {
+        "id":1,
+        "title":"Avatar",
+        "overview":"En un axuberante planeta llamado Pandora viven los Na'vi seres que ...",
+        "year":2009,
+        "rating":7.8,
+        "category":"Acción"
+    }
+]
 
 app = FastAPI()
 app.title = "Mi app con FastAPI"
@@ -6,5 +18,8 @@ app.version = "0.0.1"
 
 @app.get("/",tags=["home"])
 def message():
-    return "Hello World!"
+    return HTMLResponse('<h1>Hello World!</h1>')
 
+@app.get("/movies",tags=["movies"])
+def get_movies():
+    return movies
