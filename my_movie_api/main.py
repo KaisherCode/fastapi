f2e409344968d053dfbb2fb6a95e423e54cedec3
Wilson Dev <wilson.hch@hynkor.com>
2024-04-02 17:54:27 -0300

    Modificación y eliminación de datos con SQLAlchemy

--- my_movie_api/main.py
+++ my_movie_api/main.py
@@ -46,8 +46,8 @@ class User(BaseModel):
 
 class Movie(BaseModel):
     id: Optional[int] = None
-    title:str = Field(min_length=5,max_length=15)
-    overview:str = Field(min_length=15,max_length=50)
+    title:str = Field(min_length=5,max_length=20)
+    overview:str = Field(min_length=15,max_length=60)
     year:int = Field(le=2022)
     rating: float = Field(ge=1, le=10)
     category:str = Field(min_length=5,max_length=15)
@@ -108,19 +108,25 @@ def create_movies(movie: Movie) -> dict:
 
 @app.put("/movies/{id}",tags=["movies"],response_model=dict,status_code=200)
 def update_movie(id:int,movie:Movie)->dict:
-    for item in movies:
-        if item["id"]==id:
-            item["title"]==movie.title
-            item["overview"]==movie.overview
-            item["year"]==movie.year
-            item["rating"]==movie.rating
-            item["category"]==movie.category
-            return JSONResponse(status_code=200,content={"message":"Se ha modificado la película"})
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id==id).first()
+    if not result:
+        return JSONResponse(status_code=404,content={"message":"Category Not found"})
+    result.title = movie.title
+    result.overview = movie.overview
+    result.year = movie.year
+    result.rating = movie.rating
+    result.category = movie.category  
+    db.commit() 
+    return JSONResponse(status_code=200,content={"message":"Movie has been modified"})
             
 @app.delete("/movies/",tags=["movies"],response_model=dict,status_code=200)
 def delete_movie(id:int)->dict:
-    for item in movies:
-        if item["id"]==id:
-            movies.remove(item)
-            return JSONResponse(status_code=200,content={"message":"Se ha eliminado la película"})
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id==id).first()
+    if not result:
+        return JSONResponse(status_code = 404,content= {"message":"Category not found"})
+    db.delete(result)
+    db.commit()
+    return JSONResponse(status_code=200,content={"message":"Movie has been removed"})
     
